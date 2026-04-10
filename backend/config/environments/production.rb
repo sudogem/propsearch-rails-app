425ce771bed2eb6e60135153a508fb7976afcb02
r_man <sudogem@users.noreply.github.com>
2026-04-10 10:13:16 +0800

    Update production.rb

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -40,10 +40,12 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
+  config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
-
+  config.action_cable.url = "wss://propfind.onrender.com/cable"
+  config.action_cable.allowed_request_origins = [ "https://propfind.onrender.com/", /https:\/\/propfind.onrender.*/ ]
+  
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
   # config.assume_ssl = true
